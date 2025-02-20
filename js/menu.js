1b399d7b22b55fbc78a99356d72245e98d67addd
laktosterror <55037659+laktosterror@users.noreply.github.com>
2025-02-20 18:42:28 +0100

    cleanup and cards now loaded by js instead of hardcoded into html

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,15 @@
 const menuItems = [
     {
+        title: "Tofu Maki",
+        description: "Savory grilled marinated tofu, refreshing cucumber, and rich avocado, wrapped in fluffy sushi rice and nori, topped with a trendy sriracha mayo.",
+        image: "images/makitofu.webp"
+    },
+    {
+        title: "Asparagus Nigiri",
+        description: "Tender, lightly grilled asparagus atop a bed of warm, seasoned sushi rice, elegantly topped with a dollop of caviar for a luxurious burst of flavor.",
+        image: "images/asparagusnigiri.webp"
+    },
+    {
         title: "Squid Nigiri",
         description: "Delicate slices of fresh squid atop warm, seasoned sushi rice, drizzled with creamy mayo and garnished with fresh chives for a delightful burst of flavor.",
         image: "images/squidnigiri.webp"
@@ -9,16 +19,30 @@ const menuItems = [
         description: "Crispy fried tuna and creamy avocado, paired with zesty sriracha mayo, wrapped in seasoned sushi rice and nori, topped with a luxurious dollop of caviar for an exquisite finish.",
         image: "images/friedmaki.webp"
     },
-    // Add more menu items as needed
 ];
 
-function getRandomInList(list){
-    return Math.floor(Math.random() * list.length);
-}
-
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Site Loaded");
+    const menuContainer = document.getElementById('menu-container');
 
-    document.getElementById("myButton").onclick = function () {
-    }
-})
+    let rowElement = null;
+
+    menuItems.forEach((item, index) => {
+        if (index % 2 === 0) {
+            rowElement = document.createElement('div');
+            rowElement.className = 'row';
+            menuContainer.appendChild(rowElement);
+        }
+
+        const cardElement = document.createElement('div');
+        cardElement.className = 'col';
+        cardElement.innerHTML = `
+      <div class="card alt">
+        <h3>${item.title}</h3>
+        <img src="${item.image}" alt="${item.title}">
+        <p>${item.description}</p>
+      </div>
+    `;
+        rowElement.appendChild(cardElement);
+    });
+
+});
