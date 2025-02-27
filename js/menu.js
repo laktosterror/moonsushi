63a107515c66b876cd3230956f5ccd757fb768a6
laktosterror <55037659+laktosterror@users.noreply.github.com>
2025-02-27 23:34:42 +0100

    added more mock sushis and more properties for each sushi

--- js/menu.js
+++ js/menu.js
@@ -2,22 +2,44 @@ const menuItems = [
     {
         title: "Tofu Maki",
         description: "Savory grilled marinated tofu, refreshing cucumber, and rich avocado, wrapped in fluffy sushi rice and nori, topped with a trendy sriracha mayo.",
-        image: "images/makitofu.webp"
+        moreInfo: "Ingredients: fish, soy, mayo",
+        image: "images/makitofu.webp",
+        price: 120
     },
     {
         title: "Asparagus Nigiri",
         description: "Tender, lightly grilled asparagus atop a bed of warm, seasoned sushi rice, elegantly topped with a dollop of caviar for a luxurious burst of flavor.",
-        image: "images/asparagusnigiri.webp"
+        moreInfo: "Ingredients: asparagus, soy, mayo",
+        image: "images/asparagusnigiri.webp",
+        price: 125
     },
     {
         title: "Squid Nigiri",
         description: "Delicate slices of fresh squid atop warm, seasoned sushi rice, drizzled with creamy mayo and garnished with fresh chives for a delightful burst of flavor.",
-        image: "images/squidnigiri.webp"
+        moreInfo: "Ingredients: fish, soy, mayo",
+        image: "images/squidnigiri.webp",
+        price: 140
     },
     {
         title: "Fried Tuna Maki",
         description: "Crispy fried tuna and creamy avocado, paired with zesty sriracha mayo, wrapped in seasoned sushi rice and nori, topped with a luxurious dollop of caviar for an exquisite finish.",
-        image: "images/friedmaki.webp"
+        moreInfo: "Ingredients: fish, soy, mayo",
+        image: "images/friedmaki.webp",
+        price: 130
+    },
+    {
+        title: "Placeholder",
+        description: "Delicate slices of fresh squid atop warm, seasoned sushi rice, drizzled with creamy mayo and garnished with fresh chives for a delightful burst of flavor.",
+        moreInfo: "Ingredients: fish, soy, mayo",
+        image: "images/squidnigiri.webp",
+        price: 120
+    },
+    {
+        title: "Placeholder",
+        description: "Crispy fried tuna and creamy avocado, paired with zesty sriracha mayo, wrapped in seasoned sushi rice and nori, topped with a luxurious dollop of caviar for an exquisite finish.",
+        moreInfo: "Ingredients: fish, soy, mayo",
+        image: "images/friedmaki.webp",
+        price: 120
     },
 ];
 
